e9d87327312023d6322ce844641e69020a4db4fe
sktelasang990 <158090761+sktelasang990@users.noreply.github.com>
2024-08-07 23:04:18 +0530

    Update pgr10.py

--- pgr10.py
+++ pgr10.py
@@ -3,18 +3,10 @@ image = cv2.imread("p1.jpg")
 if image is None:
     print("Error: Unable to load image.")
     exit()
-
-# Apply Gaussian blur
 blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
-
-# Apply bilateral filter for smoothing
 smoothed_image = cv2.bilateralFilter(image, 9, 75, 75)
-
-# Display the original image and the processed images
 cv2.imshow("Original Image", image)
 cv2.imshow("Blurred Image", blurred_image)
 cv2.imshow("Smoothed Image", smoothed_image)
-
-# Wait for a key press and close all windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
