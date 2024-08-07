a428b63ab6db611f0b4706c633939c1e85f31122
sktelasang990 <158090761+sktelasang990@users.noreply.github.com>
2024-08-08 00:21:57 +0530

    Update pgr12.py

--- pgr12.py
+++ pgr12.py
@@ -1,26 +1,13 @@
 import cv2
 image = cv2.imread("p3.jpg")
-
 if image is None:
     print("Error: Unable to load image.")
     exit()
-
-# Load the pre-trained Haar Cascade classifier for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Detect faces in the grayscale image
-faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-# Draw rectangles around the detected faces
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-# Display the image with detected faces
 cv2.imshow("Detected Faces", image)
-
-# Wait for a key press and close all windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
