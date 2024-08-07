ddd266aeb0bd91e6b3f31828d124a13b5750825c
sktelasang990 <158090761+sktelasang990@users.noreply.github.com>
2024-08-08 00:47:42 +0530

    Update pgr9.py

--- pgr9.py
+++ pgr9.py
@@ -4,13 +4,9 @@ image = cv2.imread("p1.jpg")
 if image is None:
     print("Error: Unable to load image.")
     exit()
-# Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# Apply Gaussian blur to reduce noise
 blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-# Edge detection using Canny edge detector
 edges = cv2.Canny(blurred_image, 50, 150)
-# Texture extraction using Laplacian filter
 laplacian = cv2.Laplacian(blurred_image, cv2.CV_64F)
 cv2.imshow("Original Image", image)
 cv2.imshow("Edges", edges)
