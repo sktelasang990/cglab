6df355ee645b499096b26b25697edd7bb5aa451d
sktelasang990 <158090761+sktelasang990@users.noreply.github.com>
2024-08-08 17:32:31 +0530

    Update pgr6.py

--- pgr6.py
+++ pgr6.py
@@ -1,24 +1,15 @@
 import pygame
 import sys
-
-
 pygame.init()
-
-
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Display Red Triangle")
-
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-
-
 running = True
-
 while running:
     for event in pygame.event.get():
-        
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -30,10 +21,6 @@ while running:
                 ])
             elif event.button == 3:  
                 running = False
-
-    
     pygame.display.flip()
-
-
 pygame.quit()
 sys.exit()
