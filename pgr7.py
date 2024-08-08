5230f0a62adc2f33a2e2bded39a3fcb18a7cf425
sktelasang990 <158090761+sktelasang990@users.noreply.github.com>
2024-08-08 17:06:43 +0530

    Update pgr7.py

--- pgr7.py
+++ pgr7.py
@@ -1,16 +1,13 @@
 import cv2
 image = cv2.imread("p3.jpg")
-
 if image is None:
     print("Error: Unable to load image.")
     exit()
-
 height, width, _ = image.shape
 left_quadrant = image[:, :width // 2]
 right_quadrant = image[:, width // 2:]
 top_quadrant = image[:height // 2, :]
 bottom_quadrant = image[height // 2:, :]
-
 cv2.imshow("Left Quadrant", left_quadrant)
 cv2.imshow("Right Quadrant", right_quadrant)
 cv2.imshow("Top Quadrant", top_quadrant)
